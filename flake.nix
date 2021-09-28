fb32b31e4143dc89d4e3cca372623329a2634aa4
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2021-09-28 12:57:28 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "VMT - toy OS written in Zig";
+  nixConfig.bash-prompt = "\[bluepill-develop\]$ ";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShell = pkgs.mkShell { buildInputs = [ pkgs.zig ]; };
+        }
+      );
+}
