b042d1a66790dc38a1bb6391641aefa4207b66c6
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2022-01-31 22:36:12 +0100

    flake reformat

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
         {
-          devShell = pkgs.mkShell { buildInputs = [ pkgs.zig ]; };
+          devShell = pkgs.mkShell { buildInputs = with pkgs; [ zig nixpkgs-fmt ]; };
         }
       );
 }
